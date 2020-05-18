36a1b78e4bd3c3f52f040fce9eec8ab352f0f2ae
agchieppe <62956486+agchieppe@users.noreply.github.com>
2020-05-18 11:42:26 -0300

    bug no jogador

--- meu_jogo/programa.py
+++ meu_jogo/programa.py
@@ -22,7 +22,7 @@ altura = 600
 #Gera tela:
 janela = pg.display.set_mode((largura, altura))
 pg.display.set_caption('Kung-flu')
-janela_aberta = True
+game = True
 
 #Carrega as imagens:
 assets = {}
@@ -31,8 +31,9 @@ assets['personagem1'] = pg.image.load('imagens/personagem 1 menor.png').convert_
 lsObstaculos = []
 for i in range(3):
     filename = 'imagens/obstaculos/ob0{}.png'.format(i)
-    img = pg.image.load(filename).convert.aplha()
+    img = pg.image.load(filename).convert_alpha()
     img = pg.transform.scale(img, (50,50))
+    lsObstaculos.append(img)
 assets['obstaculos'] = lsObstaculos
 
 #Carrega os sons:
@@ -45,7 +46,7 @@ class obstaculo(pg.sprite.Sprite):
     def __init__(self, assets):
         pg.sprite.Sprite.__init__(self)
 
-        self.image = assets['obstaculos'][randint(0,3)] #Alterar randint para quantidade de obstaculos
+        self.image = assets['obstaculos'][randint(0,2)] #Alterar randint para quantidade de obstaculos
         self.rect = self.image.get_rect()
         self.rect.x = randint(largura, 2000)
         self.rect.y = randint(0, altura)
@@ -63,16 +64,18 @@ class obstaculo(pg.sprite.Sprite):
             self.speedy = randint(2, 9)
 
 class biroliro(pg.sprite.Sprite):
-    def __init__(self, assets):
+    def __init__(self, groups, assets):
         pg.sprite.Sprite.__init__(self)
 
-        self.image = assets['persongem1']
+        self.image = assets['personagem1']
         self.rect = self.image.get_rect()
         self.rect.centerx = 0.1*largura
-        self.rect.bottom = height/2
-        self.velocidade = 15
+        self.rect.bottom = altura/2
+        self.speedx = 0
+        self.speedy = 0
     def update(self):
-        self.rect.x += self.velocidade
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
         if self.rect.right > largura:
                 self.rect.right = largura
         if self.rect.left < 0:
@@ -87,36 +90,56 @@ class biroliro(pg.sprite.Sprite):
 #Criando grupo de obstaculos:
 all_sprites = pg.sprite.Group()
 all_obstaculos = pg.sprite.Group()
+groups = {}
+groups['all_sprites'] = all_sprites
+groups['all_obstaculos'] = all_obstaculos
+
+#Jogador:
+jogador = biroliro(groups, assets)
+all_sprites.add(jogador)
+
+#Obstaculos:
+for i in range(3): #Mudar para quantidade de obstaculos
+    obs = obstaculo(assets)
+    all_sprites.add(obs)
+    all_obstaculos.add(obs)
+
+
 #Codigo base para o relogio (placar):
 font= pg.font.SysFont(None, 30) # nao consegui botar uma fonte do meu pc
 lugardotexto= (50,50)
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 FPS = 30
 minutes = 0
 seconds = 0
 milliseconds = 0
 
 #Loop principal:
-pygame.mixer.music.play(loops=-1)
-while janela_aberta:
+pg.mixer.music.play(loops=-1)
+while game:
     clock.tick(FPS)
 
     #Checa eventos:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            janela_aberta = False
+            game = False
     comandos = pg.key.get_pressed()
     if comandos[pg.K_UP] and y >= 0:
-        y-= velocidade
+        jogador.speedx = 15
     if comandos[pg.K_DOWN] and y <= 510:
-        y+= velocidade
+        jogador.speedy = 15
     if comandos[pg.K_RIGHT] and x <= 880:
-        x+= velocidade
+        jogador.speedx = -15
     if comandos[pg.K_LEFT] and x >= 0:
-        x-= velocidade
+        jogador.speedy = -15
     
     #Atualiza posicao dos obstaculos:
-
+    all_sprites.update()
+    ai = pg.sprite.spritecollide(jogador, all_obstaculos, True)
+    if len(ai) > 0:
+        jogador.kill()
+        game = False
+        
     #Contador: fonte: https://stackoverflow.com/questions/23717803/i-need-to-make-a-stopwatch-with-pygame
     if milliseconds > 1000:
         seconds += 1
@@ -129,11 +152,9 @@ while janela_aberta:
     contador = font.render ('Tempo decorrido: {}:{}'.format(minutes, seconds), False,(255, 255, 255), (0,0,0))
 
 
-    janela.blit(fundo,(0,0))
+    janela.blit(assets['fundo'],(0,0))
     janela.blit(personagem1, (x,y))
-    janela.blit(ob1, (ob1x, ob1y))
-    #janela.blit(ob2, (ob2x, ob2y))
-    #janela.blit(ob3, (ob3x, ob3y))
+    all_sprites.draw(janela)
     janela.blit(contador, lugardotexto)
 
 
